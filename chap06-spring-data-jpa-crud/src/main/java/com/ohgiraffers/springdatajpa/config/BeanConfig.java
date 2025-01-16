cbccc4a819827f308ef3f76e9abfa583d619422e
playdrew <playdrew776p@gmail.com>
2025-01-16 17:24:51 +0900

    [2025-01-16]

--- chap06-spring-data-jpa-crud/src/main/java/com/ohgiraffers/springdatajpa/config/BeanConfig.java
+++ chap06-spring-data-jpa-crud/src/main/java/com/ohgiraffers/springdatajpa/config/BeanConfig.java
@@ -13,16 +13,33 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackages = "com.ohgiraffers.springdatajpa")
 public class BeanConfig {
 
-    @Bean
-    public ModelMapper modelMapper(){
+//    @Bean
+//    public ModelMapper modelMapper(){
+//
+//        ModelMapper modelMapper = new ModelMapper();
+//        modelMapper.getConfiguration()
+//                   .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
+//                   .setFieldMatchingEnabled(true);
+//
+//        return modelMapper;
+//
+//    }
 
+    @Bean
+    public ModelMapper modelMapper() {
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration()
-                   .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
-                   .setFieldMatchingEnabled(true);
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
+                .setFieldMatchingEnabled(true);
+
+        // Custom mapping 설정
+        modelMapper.addMappings(new PropertyMap<Menu, MenuDTO>() {
+            @Override
+            protected void configure() {
+                map().setCategoryCode(source.getCategoryCode().getCategoryCode()); // 명시적으로 매핑
+            }
+        });
 
         return modelMapper;
-
     }
-
 }
