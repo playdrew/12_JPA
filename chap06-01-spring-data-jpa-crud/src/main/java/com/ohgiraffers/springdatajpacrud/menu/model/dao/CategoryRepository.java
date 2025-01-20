e28d73bfe8e505bea1d2306e2d265b054f2fd9f7
MyComputer <playdrew776p@gmail.com>
2025-01-20 20:36:35 +0900

    [-]

--- chap06-01-spring-data-jpa-crud/src/main/java/com/ohgiraffers/springdatajpacrud/menu/model/dao/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ohgiraffers.springdatajpacrud.menu.model.dao;
-
-import com.ohgiraffers.springdatajpacrud.menu.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoryRepository extends JpaRepository<Category,Integer> {
-}
